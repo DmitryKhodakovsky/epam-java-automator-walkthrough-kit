dc397ca4e033d68b22be5815da2362e2b5e74412
Dmitry Khodakovsky <khda91@gmail.com>
2021-04-15 10:28:17 +0300

    added screenshot

--- src/test/java/com/epam/java/automator/walkthrought/kit/hooks/CucumberHooks.java
+++ src/test/java/com/epam/java/automator/walkthrought/kit/hooks/CucumberHooks.java
@@ -4,6 +4,7 @@ import com.epam.java.automator.walkthrought.kit.service.driver.DriverSingleton;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
+import io.qameta.allure.Allure;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.MDC;
 
@@ -16,7 +17,13 @@ public class CucumberHooks {
   }
 
   @After
-  public void tearDown() {
+  public void tearDown(Scenario scenario) {
+    if (scenario.isFailed()) {
+      byte[] screenshot = DriverSingleton.getDriver().takeScreenshot();
+      Allure.getLifecycle().addAttachment("failed_screenshot", "image/png", "png", screenshot);
+      String pageSource = DriverSingleton.getDriver().getPageSource();
+      Allure.addAttachment("failed_page_source", "text/html", pageSource, "html");
+    }
     DriverSingleton.closeDriver();
   }
 
